f730ba31f386983cc7ff2e12e77646c177e57590
Daniel.xiao <dxweixiao@tencent.com>
2022-08-08 14:52:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# nc-auto-test
+
+## Install
+```sh
+$ npm i nc-auto-test -g # yarn nc-auto-test add ncgen
+```
+
+## Usage
+```sh
+$ nc-auto-test -h
+```
+
+## Develop
+```sh
+# Debug main command
+$ node bin/cli
+
+# Debug subcommand
+$ node bin/cli subcommand-name
+```
+
+## Publish
+```sh
+$ npm run release
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+import { generate } from "ncgen";
+import program from "commander";
+import updateNotifier from "update-notifier";
+
+import ncgenConfig from "./ncgen-config";
+import pkg from "./package.json";
+
+export function main(args) {
+
+  checkVersion()
+
+  program.version(pkg.version, "-V, --version").usage("<command>");
+
+  // subcommands
+  Object.keys(ncgenConfig.sub).forEach(subcommand => {
+    program
+      .command(subcommand)
+      .description(ncgenConfig.sub[subcommand].description)
+      .action(async () => {
+        await generate(ncgenConfig, { type: "s", command: subcommand });
+      });
+  });
+
+  // main command
+  program.action(async () => {
+    await generate(ncgenConfig, { type: "m" });
+  });
+
+  program.parse(args);
+}
+
+function checkVersion() {
+  const notifier = updateNotifier({ pkg, updateCheckInterval: 0 });
+
+  if (notifier.update) {
+    notifier.notify();
+  }
+}
